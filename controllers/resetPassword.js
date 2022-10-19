9af0a71819603fdd5908642fe7d7bcd7a4d8a1df
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2022-10-19 17:55:43 +0100

    j

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -1,29 +1,33 @@
 const User = require("../models/userModel");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const resetPassword = async (req, res) => {
-  const {password} = req.body
-  const token = req.bearer.authorization.split(" ")[1];
+  const { password } = req.body;
+  if (!password) {
+    res.status(400).json({
+      message: "enter all fields",
+    });
+  } else {
+    try {
+      const token = req.bearer.authorization.split(" ")[1];
 
-  const { id } = jwt.verify(token, process.env.JWT_PASS);
-
-  try {
-    const user = await User.findById(id);
-const secret = await  bcrypt.genSalt(10)
-const hashedPassword = await bcrypt.hash(password, secret)
-user.password = hashedPassword
-    const update = await user.save()
-    if (update) {
+      const { id } = jwt.verify(token, process.env.JWT_PASS);
+      const user = await User.findById(id);
+      const secret = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, secret);
+      user.password = hashedPassword;
+      const update = await user.save();
+      if (update) {
+        res.status(401).json({
+          message: "Password Changed.",
+        });
+      }
+    } catch (error) {
       res.status(401).json({
-        message: "Password Changed.",
+        message: "Not authorized to accesss this route",
       });
     }
-  } catch (error) {
-    res.status(401).json({
-      message: "Please enter new password",
-    });
   }
 };
-
 module.exports = resetPassword;

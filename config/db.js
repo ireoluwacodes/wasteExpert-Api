0b7ef123e9bb25e87e3056a4cf6abce6e6ea344d
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2022-10-19 16:15:46 +0100

    changes

--- config/db.js
+++ config/db.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
+const selectDB = ( ) => {
+  if(process.env.NODE_ENV === "production"){
+      return process.env.MONGO_URI
+  }else{
+    return process.env.LOCAL_MONGO_URL
+  }
+}
+
 
 const createDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.LOCAL_MONGO_URL);
+    const connect = await mongoose.connect(selectDB());
     console.log(
       `Database connected sucessfully to ${connect.connection.host}`.blue
         .underline.bold

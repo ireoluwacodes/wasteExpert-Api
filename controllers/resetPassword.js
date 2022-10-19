788050806335711799e7af1f13afa42fc96ea850
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2022-10-19 18:00:43 +0100

    b

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -26,6 +26,7 @@ const resetPassword = async (req, res) => {
     } catch (error) {
       res.status(401).json({
         message: "Not authorized to accesss this route",
+        error,
       });
     }
   }

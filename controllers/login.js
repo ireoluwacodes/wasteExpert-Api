0700e5480d7588c80e1e7f40207fae5885723307
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2022-10-21 11:08:07 +0100

    changes

--- controllers/login.js
+++ controllers/login.js
@@ -5,39 +5,43 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const login = async (req, res) => {
-const {email, password} =  req.body
-if(!email || !password){
+  const { email, password } = req.body;
+  if (!email || !password) {
     res.status(401).json({
-        message : "please enter all feilds"
-    })
-}else{
+      message: "please enter all feilds",
+    });
+  } else {
     try {
-       const user = await User.findOne({email})
+      const user = await User.findOne({ email });
 
-       const id = user._id
+      const id = user._id;
 
-       if(user.isVerified){
-       const validate = await bcrypt.compare(password, user.password)
-        if(validate){
-            const token = jwt.sign({id}, process.env.JWT_PASS, {
-                expiresIn : "1d"
-               })
-               res.status(200).json({
-                message : "Login Successfully",
-                token,
-            })
+      if (user.isVerified) {
+        const validate = await bcrypt.compare(password, user.password);
+        if (validate) {
+          const token = jwt.sign({ id }, process.env.JWT_PASS, {
+            expiresIn: "1d",
+          });
+          res.status(200).json({
+            message: "Login Successfully",
+            token,
+          });
+        } else {
+          res.status(401).json({
+            message: "incorrect password",
+          });
         }
-       } else{
+      } else {
         res.status(401).json({
-            message : "Please verify your email"
-        })
-       }
+          message: "Please verify your email",
+        });
+      }
     } catch (error) {
-        res.status(401).json({
-            message : "User does not exist",
-        })
+      res.status(401).json({
+        message: "User does not exist",
+      });
     }
-}
-}
+  }
+};
 
-module.exports = login
+module.exports = login;

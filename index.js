3e967e909071aadd7ef515e1bbce8d6a1185985a
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2023-02-25 10:34:27 +0100

    swagger css

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const notFound = require("./middlewares/notFound");
 
 const app = express();
 
+const options = { customCssUrl: '/public/css/swagger-ui.css',};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs, options));
 app.use("/api/v1/auth", userRoute);
 
 app.get("/", (req, res) => {
